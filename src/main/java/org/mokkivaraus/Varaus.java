f858575e496a39656fbe30a71f6188254f8d3bb7
ComplexMonkeys <102974554+ComplexMonkeys@users.noreply.github.com>
2022-04-21 09:43:43 +0300

    varaus.java

---
+++ src/main/java/org/mokkivaraus/Varaus.java
@@ -0,0 +1,67 @@
+package org.mokkivaraus;
+
+public class Varaus{
+private int varausId;
+private int asiakasId;
+private int mokkiId;
+private String varattuPvm;
+private String vahvistusPvm;
+private String varattuAlku;
+private String varattuLoppu;
+
+// Alustaja joka saa kaikkiin kenttiin inputin.
+public Varaus(int varausId, int asiakasId, int mokkiId, String varattuPvm, String vahvistusPvm, String varattuAlku,
+        String varattuLoppu) {
+    this.varausId = varausId;
+    this.asiakasId = asiakasId;
+    this.mokkiId = mokkiId;
+    this.varattuPvm = varattuPvm;
+    this.vahvistusPvm = vahvistusPvm;
+    this.varattuAlku = varattuAlku;
+    this.varattuLoppu = varattuLoppu;
+}
+public int getVarausId() {
+    return varausId;
+}
+public void setVarausId(int varausId) {
+    this.varausId = varausId;
+}
+public int getAsiakasId() {
+    return asiakasId;
+}
+public void setAsiakasId(int asiakasId) {
+    this.asiakasId = asiakasId;
+}
+public int getMokkiId() {
+    return mokkiId;
+}
+public void setMokkiId(int mokkiId) {
+    this.mokkiId = mokkiId;
+}
+public String getVarattuPvm() {
+    return varattuPvm;
+}
+public void setVarattuPvm(String varattuPvm) {
+    this.varattuPvm = varattuPvm;
+}
+public String getVahvistusPvm() {
+    return vahvistusPvm;
+}
+public void setVahvistusPvm(String vahvistusPvm) {
+    this.vahvistusPvm = vahvistusPvm;
+}
+public String getVarattuAlku() {
+    return varattuAlku;
+}
+public void setVarattuAlku(String varattuAlku) {
+    this.varattuAlku = varattuAlku;
+}
+public String getVarattuLoppu() {
+    return varattuLoppu;
+}
+public void setVarattuLoppu(String varattuLoppu) {
+    this.varattuLoppu = varattuLoppu;
+}
+
+
+}
