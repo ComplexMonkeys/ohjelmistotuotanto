f0ff0e4f242a16cb7821da3012d177a0eb33010b
Kimboooo <102971438+Kimboooo@users.noreply.github.com>
2022-04-21 12:04:39 +0300

    tallennabutton:-)

--- src/main/java/org/mokkivaraus/controller/lisaaPalveluIkkunaController.java
+++ src/main/java/org/mokkivaraus/controller/lisaaPalveluIkkunaController.java
@@ -1,9 +1,15 @@
 package org.mokkivaraus.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.sql.*;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class lisaaPalveluIkkunaController {
 
@@ -36,12 +42,39 @@ public class lisaaPalveluIkkunaController {
 
     @FXML
     void btPeruutaAction(ActionEvent event) {
-
+        Stage stage = (Stage) btPeruuta.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
-    void btTallennaAction(ActionEvent event) {
+    void btTallennaAction(ActionEvent event) throws Exception {
+        if (tfAlueId.getText() != "" && tfAlv.getText() != "" && tfHinta.getText() != "" && tfKuvaus.getText() != ""
+        && tfNimi.getText() != "" && tfOsoite.getText() != "" && tfTyyppi.getText() != ""){
 
+            Connection con = DriverManager.getConnection(
+                // Tässä asetetaan tietokannan tiedot, osoite, käyttäjätunnus, salasana.
+                "jdbc:mysql://localhost:3306/vn", "employee", "password");
+            try{
+                java.sql.Statement stmt = con.createStatement();
+                int alueenid = Integer.parseInt(tfAlueId.getText());
+                int alv = Integer.parseInt(tfAlv.getText());
+                int hinta = Integer.parseInt(tfHinta.getText());
+                String kuvaus = tfKuvaus.getText();
+                String nimi = tfNimi.getText();
+                String osoite = tfOsoite.getText();
+                String tyyppi = tfTyyppi.getText();
+
+                stmt.executeUpdate(
+                    "INSERT INTO palvelu (alue_id, alv, hinta, kuvaus, nimi, osoite, tyyppi) VALUES('"+ alueenid + "','" + alv + "','" +  hinta + "','" + kuvaus + "','" + nimi + "','" + osoite + "','" + tyyppi + "');");
+                
+                 } catch(Exception e){
+                     System.out.println(e);
+                 } finally{
+                     con.close();
+                 }
+                 Stage stage = (Stage) btTallenna.getScene().getWindow();
+                 stage.close();
+        }
     }
 
 }
