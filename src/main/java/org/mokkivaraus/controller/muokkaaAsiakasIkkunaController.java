24a5ae08ffed456a726353868b1f7949fe325272
heikamal <102971293+heikamal@users.noreply.github.com>
2022-04-21 10:22:25 +0300

    lisaaAsiakas ja muokkaaAsiakas-ikkunat tehtynä

---
+++ src/main/java/org/mokkivaraus/controller/muokkaaAsiakasIkkunaController.java
@@ -0,0 +1,48 @@
+package org.mokkivaraus.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class muokkaaAsiakasIkkunaController {
+
+    @FXML
+    private Button btPeruuta;
+
+    @FXML
+    private Button btTallenna;
+
+    @FXML
+    private Label labelId;
+
+    @FXML
+    private TextField tfEmail;
+
+    @FXML
+    private TextField tfEtunimi;
+
+    @FXML
+    private TextField tfOsoite;
+
+    @FXML
+    private TextField tfPostiNro;
+
+    @FXML
+    private TextField tfPuhNro;
+
+    @FXML
+    private TextField tfSukunimi;
+
+    @FXML
+    void btPeruutaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btTallennaAction(ActionEvent event) {
+
+    }
+
+}
