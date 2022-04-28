2543abf435066f04078f8dde8180029beae95986
Potakka <102971224+Potakka@users.noreply.github.com>
2022-04-28 14:31:06 +0300

    varauksen muokkaus toimii nyt

--- src/main/java/org/mokkivaraus/controller/muokkaaVarausIkkunaController.java
+++ src/main/java/org/mokkivaraus/controller/muokkaaVarausIkkunaController.java
@@ -2,6 +2,7 @@ package org.mokkivaraus.controller;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.time.format.DateTimeFormatter;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,6 +10,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class muokkaaVarausIkkunaController {
@@ -45,24 +47,35 @@ public class muokkaaVarausIkkunaController {
      Stage stage = (Stage) btPeruuta.getScene().getWindow();
      stage.close();
     }
+    
 
     @FXML
     void btTallennaAction(ActionEvent event) throws Exception {
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/vn", "employee", "password");
         try{
             java.sql.Statement stmt = con.createStatement();
-            int palvelunid = Integer.parseInt(labelId.getText());
-            int asiakasId = Integer.parseInt(tfAsiakas.getText());
-            int mokkiId = Integer.parseInt(tfMokki.getText());
+            int varauksenid = Integer.parseInt(labelId.getText());
+            int asiakkaanid = Integer.parseInt(tfAsiakas.getText());
+            String aloituspvm = dpAloitus.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            String lopetuspvm = Lopetus.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            String vahvistuspvm = dpVahvistus.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            int mokinid = Integer.parseInt(tfMokki.getText());
 
-            stmt.executeUpdate("UPDATE varaus set palvelu_id = "+ palvelunid +",asiakas_id = "+ asiakasId +",mokki_id = "+ mokkiId +";");
+            stmt.executeUpdate("UPDATE varaus set asiakas_id = "+ asiakkaanid +", mokki_mokki_id = "+ mokinid +" , vahvistus_pvm = '"+ vahvistuspvm +"' , varattu_alkupvm = '"+ aloituspvm +"' , varattu_loppupvm = '"+ lopetuspvm +"' where varaus_id = "+ varauksenid +";");
         }
         catch(Exception e){
             System.out.println(e);
+            Alert constraitAlert = new Alert(AlertType.ERROR);
+            constraitAlert.setHeaderText("Jotain meni vikaan");
+            constraitAlert.setContentText("Tarkista, että asiakas ja mökki ovat olemassa");
+            constraitAlert.showAndWait();
+
         }
         finally{
             con.close();
         }
+        Stage stage = (Stage) btTallenna.getScene().getWindow();
+        stage.close();
     }
     public void initdata(int varausId, String Varattupvm, int mokkiId, int asiakasId) {
         labelId.setText(Integer.toString(varausId));
