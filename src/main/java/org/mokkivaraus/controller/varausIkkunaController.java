1c6e702cc929bf4cd5c6480119bf6952b3a1f971
ComplexMonkeys <102974554+ComplexMonkeys@users.noreply.github.com>
2022-04-28 12:33:33 +0300

    lisätty listan toiminnallisuus

--- src/main/java/org/mokkivaraus/controller/varausIkkunaController.java
+++ src/main/java/org/mokkivaraus/controller/varausIkkunaController.java
@@ -1,21 +1,33 @@
 package org.mokkivaraus.controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-import org.mokkivaraus.Mokinvaraus;
+import org.mokkivaraus.*;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public class varausIkkunaController {
+public class varausIkkunaController implements Initializable {
 
     @FXML
     private Button btLisaa;
@@ -33,22 +45,33 @@ public class varausIkkunaController {
     private Button btPoista;
 
     @FXML
-    private TableColumn<?, ?> cAloitusPvm;
+    private TableColumn<Varaus, String> cAloitusPvm;
 
     @FXML
-    private TableColumn<?, ?> cMokkiId;
+    private TableColumn<Varaus, Integer> cMokkiId;
 
     @FXML
-    private TableColumn<?, ?> cPaatosPvm;
+    private TableColumn<Varaus, String> cPaatosPvm;
 
     @FXML
-    private TableColumn<?, ?> cVarausId;
+    private TableColumn<Varaus, Integer> cVarausId;
 
     @FXML
     private HBox hbNapit;
 
     @FXML
-    private TableView<?> tvVaraus;
+    private TableView<Varaus> tvVaraus;
+
+    Varaus valittu;
+
+    public void paivitaVarauslista() {
+        try {
+            tvVaraus.getItems().setAll(haeVarauslista());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     @FXML
     void btLisaaAction(ActionEvent event) {
@@ -82,6 +105,55 @@ public class varausIkkunaController {
     @FXML
     void btPoistaAction(ActionEvent event) {
 
+    }
+
+    public List<Varaus> haeVarauslista() throws SQLException {
+        List<Varaus> lista = new ArrayList<>();
+
+        try {
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/vn", "employee", "password");
+            java.sql.Statement stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM varaus");
+
+            while (rs.next()) {
+                Varaus tempalue = new Varaus(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getString(4), rs.getString(5),
+                        rs.getString(6), rs.getString(7));
+                lista.add(tempalue);
+            }
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return lista;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        cMokkiId.setCellValueFactory(new PropertyValueFactory<Varaus, Integer>("mokkiId"));
+        cVarausId.setCellValueFactory(new PropertyValueFactory<Varaus, Integer>("VarausId"));
+        cPaatosPvm.setCellValueFactory(new PropertyValueFactory<Varaus, String>("VarattuAlku"));
+        cAloitusPvm.setCellValueFactory(new PropertyValueFactory<Varaus, String>("VarattuLoppu"));
+
+        try {
+            tvVaraus.getItems().setAll(haeVarauslista());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        tvVaraus.setRowFactory(tv -> {
+            TableRow<Varaus> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (!row.isEmpty() && event.getButton() == MouseButton.PRIMARY) {
+
+                    valittu = row.getItem();
+                    btMuokkaa.setDisable(false);
+                    btPoista.setDisable(false);
+                }
+            });
+            return row;
+        });
     }
 
 }
