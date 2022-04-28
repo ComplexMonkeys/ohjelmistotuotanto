a66b81d66fed5c75189b8e70e2bc9d6b04917de6
Potakka <102971224+Potakka@users.noreply.github.com>
2022-04-28 14:31:53 +0300

    korjattu alert box

--- src/main/java/org/mokkivaraus/controller/muokkaaVarausIkkunaController.java
+++ src/main/java/org/mokkivaraus/controller/muokkaaVarausIkkunaController.java
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
