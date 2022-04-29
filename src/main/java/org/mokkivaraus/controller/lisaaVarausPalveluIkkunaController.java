1d766f4ac8fd026aa8b600fd5d667dab4db268fb
Potakka <102971224+Potakka@users.noreply.github.com>
2022-04-29 13:18:25 +0300

    peruuta nappi tehty jälleen

--- src/main/java/org/mokkivaraus/controller/lisaaVarausPalveluIkkunaController.java
+++ src/main/java/org/mokkivaraus/controller/lisaaVarausPalveluIkkunaController.java
@@ -36,7 +36,8 @@ public class lisaaVarausPalveluIkkunaController {
 
     @FXML
     void btPeruutaAction(ActionEvent event) {
-
+        Stage stage = (Stage) btPeruuta.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
