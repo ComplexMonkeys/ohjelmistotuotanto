f191b1723350f2dcdcab6a2405a39152df6058f9
heikamal <102971293+heikamal@users.noreply.github.com>
2022-05-02 13:26:54 +0300

    turhia importteja siistitty

--- src/main/java/org/mokkivaraus/Mokki.java
+++ src/main/java/org/mokkivaraus/Mokki.java
@@ -1,8 +1,6 @@
 package org.mokkivaraus;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Mokki {
     // mökin id
--- src/main/java/org/mokkivaraus/controller/mokitIkkunaController.java
+++ src/main/java/org/mokkivaraus/controller/mokitIkkunaController.java
@@ -199,7 +199,7 @@ public class mokitIkkunaController implements Initializable {
             TableRow<Mokki> row = new TableRow<>();
             row.setOnMouseClicked(event -> {
                 if (!row.isEmpty() && event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2) {
-
+                    // TODO: Heikki tekee kivoja juttuja.
                     tulostus = row.getItem();
                     System.out.println(tulostus);
                 }
