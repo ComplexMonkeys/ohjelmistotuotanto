62be43f310f5b8569f5319a13e0213b5c8bc0bef
heikamal <102971293+heikamal@users.noreply.github.com>
2022-05-09 14:55:23 +0300

    Javadoc-kommentit

--- src/main/java/org/mokkivaraus/Asiakas.java
+++ src/main/java/org/mokkivaraus/Asiakas.java
@@ -1,91 +1,227 @@
 package org.mokkivaraus;
 
+/**
+ * Luokka asiakkaan tietojen olioon tallettamiseen tietokannasta.
+ * Ottaa muuttujikseen tietokannan asiakas-taulun vastaavat ominaisuudet.
+ */
 public class Asiakas {
 
-private int asiakas_id;
+    /**
+     * Asiakkaan ID
+     */
+    private int asiakas_id;
 
-private String postinro;
+    /**
+     * Asiakkaan postinumero
+     */
+    private String postinro;
 
-private String etunimi;
+    /**
+     * Asiakkaan etunimi
+     */
+    private String etunimi;
 
-private String sukunimi;
+    /**
+     * Asiakkaan sukunimi
+     */
+    private String sukunimi;
 
-private String lahiosoite;
+    /**
+     * Asiakkaan lähiosoite
+     */
+    private String lahiosoite;
 
-private String email;
+    /**
+     * Asiakkaan sähköpostiosoite
+     */
+    private String email;
 
-private String puhelinnro;
+    /**
+     * Asiakkaan puhelinnumero
+     */
+    private String puhelinnro;
 
-private String nimi;
+    /**
+     * Asiakkaan koko nimi merkkijonona, muodostuu etunimi + sukunimi
+     */
+    private String nimi;
 
+    /**
+     * Parametritön alustaja
+     */
+    public Asiakas() {
+    }
 
-//parametriton alustaja
-public Asiakas(){
-}
-//parametrillinen alustaja
-public Asiakas(int asiakas_id,String postinro,String etunimi,String sukunimi,
-String lahiosoite, String email, String puhelinnro){
-this.asiakas_id = asiakas_id;
-this.postinro = postinro;
-this.etunimi = etunimi;
-this.sukunimi = sukunimi;
-this.lahiosoite = lahiosoite;
-this.email = email;
-this.puhelinnro = puhelinnro;
-this.nimi = etunimi + " " + sukunimi;
-}
-public int getAsiakas_id() {
-    return asiakas_id;
-}
-public void setAsiakas_id(int asiakas_id) {
-    this.asiakas_id = asiakas_id;
-}
-public String getPostinro() {
-    return postinro;
-}
-public void setPostinro(String postinro) {
-    this.postinro = postinro;
-}
-public String getEtunimi() {
-    return etunimi;
-}
-public void setEtunimi(String etunimi) {
-    this.etunimi = etunimi;
-}
-public String getSukunimi() {
-    return sukunimi;
-}
-public void setSukunimi(String sukunimi) {
-    this.sukunimi = sukunimi;
-}
-public String getLahiosoite() {
-    return lahiosoite;
-}
-public void setLahiosoite(String lahiosoite) {
-    this.lahiosoite = lahiosoite;
-}
-public String getEmail() {
-    return email;
-}
-public void setEmail(String email) {
-    this.email = email;
-}
-public String getPuhelinnro() {
-    return puhelinnro;
-}
-public void setPuhelinnro(String puhelinnro) {
-    this.puhelinnro = puhelinnro;
-}
-public String getNimi(){
-    return nimi;
-}
-@Override
-public String toString(){
-    return "Asiakkaan ID: " + getAsiakas_id() + "\n" +
-    "Nimi: " + getNimi() + "\n" +
-    "Lähiosoite: " + getLahiosoite() + "\n" +
-    "Postinumero: " + getPostinro() + "\n" +
-    "Sähköpostiosoite: " + getEmail() + "\n" +
-    "Puh. numero: " + getPuhelinnro();
-}
+    /**
+     * Parametrillinen alustaja. 
+     * Asettaa asiakkaan nimen yhdistämällä etunimen ja sukunimen yhdeksi merkkijonokseen.
+     * 
+     * @param asiakas_id Asiakkaan ID
+     * @param postinro Asiakkaan postinumero
+     * @param etunimi Asiakkaan etunimi
+     * @param sukunimi Asiakkaan sukunimi
+     * @param lahiosoite Asiakkaan osoite
+     * @param email Asiakkaan sähköpostiosoite
+     * @param puhelinnro Asiakkaan puhelinnumero
+     */
+    public Asiakas(int asiakas_id, String postinro, String etunimi, String sukunimi,
+            String lahiosoite, String email, String puhelinnro) {
+        this.asiakas_id = asiakas_id;
+        this.postinro = postinro;
+        this.etunimi = etunimi;
+        this.sukunimi = sukunimi;
+        this.lahiosoite = lahiosoite;
+        this.email = email;
+        this.puhelinnro = puhelinnro;
+        this.nimi = etunimi + " " + sukunimi;
+    }
+
+    /**
+     * Hakumetodi asiakkaan ID:lle.
+     * 
+     * @return int Asiakkaan ID
+     */
+    public int getAsiakas_id() {
+        return asiakas_id;
+    }
+
+    /**
+     * Asetusmetodi asiakkaan ID:lle.
+     * 
+     * @param asiakas_id Asiakkaan ID
+     */
+    public void setAsiakas_id(int asiakas_id) {
+        this.asiakas_id = asiakas_id;
+    }
+
+    /**
+     * Hakumetodi postinumerolle.
+     * 
+     * @return String Asiakkaan postinumero
+     */
+    public String getPostinro() {
+        return postinro;
+    }
+
+    /**
+     * Asetusmetodi postinumerolle.
+     * 
+     * @param postinro Asiakkaan postinumero
+     */
+    public void setPostinro(String postinro) {
+        this.postinro = postinro;
+    }
+
+    /**
+     * Hakumetodi asiakkaan etunimelle.
+     * 
+     * @return String Asiakkaan etunimi
+     */
+    public String getEtunimi() {
+        return etunimi;
+    }
+
+    /**
+     * Asetusmetodi asiakkaan etunimelle.
+     * 
+     * @param etunimi Asiakkaan etunimi
+     */
+    public void setEtunimi(String etunimi) {
+        this.etunimi = etunimi;
+    }
+
+    /**
+     * Hakumetodi asiakkaan sukunimelle.
+     * 
+     * @return String Asiakkaan sukunimi
+     */
+    public String getSukunimi() {
+        return sukunimi;
+    }
+
+    /**
+     * Asetusmetodi asiakkaan sukunimelle.
+     * 
+     * @param sukunimi Asiakkaan sukunimi
+     */
+    public void setSukunimi(String sukunimi) {
+        this.sukunimi = sukunimi;
+    }
+
+    /**
+     * Hakumetodi asiakkaan osoitteelle.
+     * 
+     * @return String Asiakkaan posti-/laskutusosoite
+     */
+    public String getLahiosoite() {
+        return lahiosoite;
+    }
+
+    /**
+     * Asetusmetodi asiakkaan osoitteelle.
+     * 
+     * @param lahiosoite Asiakkaan osoite
+     */
+    public void setLahiosoite(String lahiosoite) {
+        this.lahiosoite = lahiosoite;
+    }
+
+    /**
+     * Hakumetodi asiakkaan sähköpostiosoitteelle.
+     * 
+     * @return String Asiakkaan sähköpostiosoite
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Asetusmetodi asiakkaan sähköpostiosoitteelle.
+     * 
+     * @param email Asiakkaan sähköpostiosoite
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Hakumetodi asiakkaan puhelinnumerolle.
+     * 
+     * @return String Asiakkaan puhelinnumero
+     */
+    public String getPuhelinnro() {
+        return puhelinnro;
+    }
+
+    /**
+     * Asetusmetodi asiakkaan puhelinnumerolle.
+     * 
+     * @param puhelinnro Asiakkaan puhelinnumero
+     */
+    public void setPuhelinnro(String puhelinnro) {
+        this.puhelinnro = puhelinnro;
+    }
+
+    /**
+     * Hakumetodi asiakkaan nimelle.
+     * 
+     * @return String Asiakkaan nimi muodossa Etunimi + sukunimi
+     */
+    public String getNimi() {
+        return nimi;
+    }
+
+    /**
+     * Tostring-metodi olion tietojen tulostamiseen.
+     * @return String Olion tiedot listattuna merkkijonona
+     */
+    @Override
+    public String toString() {
+        return "Asiakkaan ID: " + getAsiakas_id() + "\n" +
+                "Nimi: " + getNimi() + "\n" +
+                "Lähiosoite: " + getLahiosoite() + "\n" +
+                "Postinumero: " + getPostinro() + "\n" +
+                "Sähköpostiosoite: " + getEmail() + "\n" +
+                "Puh. numero: " + getPuhelinnro();
+    }
 }
